c1b1ab0dd706e2aa1955fed2c3a01b372eda779c
j3am6s <95968769+j3am6s@users.noreply.github.com>
2022-07-09 11:34:44 +0900

    Create monday_quotes.py

---
+++ smtp/monday_quotes.py
@@ -0,0 +1,16 @@
+import smtplib
+import datetime as dt
+from random import choice
+
+today = dt.datetime.now()
+EMAIL = ""
+PASSWORD = ""
+
+if today.weekday()==4:
+    with open("smtp/quotes.txt","r") as file:
+        quotes = file.readlines()
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL,
+        to_addrs="manon1606manon@gmail.com",
+        msg=f"Subject:Weekly Quote\n\n{choice(quotes)}")
