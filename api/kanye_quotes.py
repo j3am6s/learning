042c427c1ade7c235cb42a28b5b65e3a38d89a64
j3am6s <95968769+j3am6s@users.noreply.github.com>
2022-07-10 21:44:31 +0900

    Update kanye_quotes.py

--- api/kanye_quotes.py
+++ api/kanye_quotes.py
@@ -8,8 +8,6 @@ def get_quote():
     quote = response.json()["quote"]
     canvas.itemconfig(quote_text, text=quote)
 
-
-
 window = Tk()
 window.title("Kanye Says...")
 window.config(padx=50, pady=50)
@@ -23,7 +21,6 @@ canvas.grid(row=0, column=0)
 kanye_img = PhotoImage(file="api/kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
 
 
 window.mainloop()
