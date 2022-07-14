7bb4428f0861ba84172213be42e90d15a864258d
j3am6s <95968769+j3am6s@users.noreply.github.com>
2022-07-14 19:37:33 +0900

    Create data_manager.py

---
+++ api/flight_club/data_manager.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+class DataManager:
+    def __init__(self):
+        headers = {
+            "Authorization": os.environ.get("authorization")
+        }
+        iata = ["PAR", "BER", "TYO", "SYD", "IST", "KUL", "NYC", "SFO", "CPT"]
+        for i in range(2, 11):
+            change = {
+                "price": {
+                    'iataCode': iata[i-2]
+                }
+            }
+            requests.put(f"https://api.sheety.co/13fe58756d09cfaa4bea89b12756f4f3/flightMinPrice/prices/{i}", json=change, headers=headers)
+        response = requests.get("https://api.sheety.co/13fe58756d09cfaa4bea89b12756f4f3/flightMinPrice/prices", headers=headers) 
+        self.data = response.json()
+        
